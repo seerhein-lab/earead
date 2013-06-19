8762dfd5f3b6a2f1c8162c3bd419793abc205a37
nkunstek <nkunstek@seitenbau.net>
2013-06-19 14:38:42 +0200

    ...

--- src/main/java/com/seitenbau/test/ReaderHelper.java
+++ src/main/java/com/seitenbau/test/ReaderHelper.java
@@ -4,21 +4,19 @@ import java.util.Iterator;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
-import org.eclipse.emf.ecore.EAttribute;
-import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.EPackage;
-import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.EPackage.Registry;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.xmi.XMLResource;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+import org.eclipse.uml2.uml.Class;
 import org.eclipse.uml2.uml.Element;
 import org.eclipse.uml2.uml.Model;
-import org.eclipse.uml2.uml.PackageableElement;
-import org.eclipse.uml2.uml.UMLPackage;
 import org.eclipse.uml2.uml.Package;
+import org.eclipse.uml2.uml.PackageableElement;
+import org.eclipse.uml2.uml.Property;
+import org.eclipse.uml2.uml.UMLPackage;
 import org.eclipse.uml2.uml.internal.resource.UMLResourceFactoryImpl;
 import org.eclipse.uml2.uml.resource.XMI2UMLResource;
 import org.eclipse.uml2.uml.util.UMLUtil;
@@ -110,67 +108,46 @@ public abstract class ReaderHelper {
 			System.out.println("Package is null.");
 			return;
 		}
-		System.out.println("\t\tPackage: " + packageElement.getName());
-
-		Iterator<EObject> allContents = UMLUtil.getAllContents(packageElement,
-				true, false);
-
-		while (allContents.hasNext()) {
-
-			EObject eObject = (EObject) allContents.next();
-
-			if (eObject instanceof Element) {
-				System.out.println("\t\tElement: " + eObject.toString());
-			}
-		}
-		
-		
-		
-		System.out.println("\n\n\n");
-
-		// TODO ab hier
-		// print packages in resource.
-		allContents = UMLUtil.getAllContents(packageElement,
-				true, false);
-		while (allContents.hasNext()) {
-
-			EObject pkge = (EObject) allContents.next();
-			if (pkge instanceof EPackage) {
-				System.out.println("Package: " + ((EPackage) pkge).getName());
+		System.out.println("Package: " + packageElement.getName());
+				Package package1 = packageElement.getNestedPackage("MKO_Bookstore");
+				System.out.println("Package: " + package1.getName());
 
 				// print entities in package.
-				for (EObject clss : pkge.eContents()) {
-					if (clss instanceof EClass) {
-						System.out.println("\tClass: "
-								+ ((EClass) clss).getName());
+				for (EObject clss : package1.eContents()) {
+					if (clss instanceof Class) {
+						Class class1 = (Class) clss;
+						System.out.println("\tClass: " + class1.getName());
 
 						// print attributes of entities.
-						for (EAttribute attr : ((EClass) clss)
-								.getEAllAttributes()) {
-							if (attr.getEType() != null) {
+						for (Property attr : class1.getAllAttributes()) {
+							if (attr.getType() != null) {
 								System.out
 										.println("\t\tKey: " + attr.getName());
 							} else {
-								System.out.println("\t\tAttribute: "
-										+ attr.getName());
+								String attribute = "\t\tAttribute: "
+										+ attr.getName();
+								if ("ATT_Name".equals(attr.getName())) {
+									if (attr.getType() != null) {
+										attribute += ":"
+												+ attr.getType().getName();
+									}
+								}
+								System.out.println(attribute);
 							}
 						}
 
 						// print references between entities.
-						for (EReference ref : ((EClass) clss)
-								.getEAllReferences()) {
-							System.out.println("\t\tOpposite Entity: "
-									+ ref.getEReferenceType().getName());
-						}
+//						for (EReference ref : ((EClass) clss)
+//								.getEAllReferences()) {
+//							System.out.println("\t\tOpposite Entity: "
+//									+ ref.getEReferenceType().getName());
+//						}
 
 					} else {
 						System.err.println("Class error!");
 					}
 				}
-			} else {
-				System.err.println("Package error!");
-			}
 
-		}
+		
 	}
 }
